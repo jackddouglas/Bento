35aa0ac0428a1f86f2c4441686d715bcf9c3f890
Jack D. Douglas <jackdouglas2@me.com>
2020-12-22 20:07:27 -0800

    update theme toggle

--- js/weather.js
+++ js/weather.js
@@ -1,54 +1,55 @@
-const iconElement = document.querySelector('.weather-icon');
-const tempElement = document.querySelector('.temperature-value p');
-const descElement = document.querySelector('.temperature-description p');
+const iconElement = document.querySelector(".weather-icon");
+const tempElement = document.querySelector(".temperature-value p");
+const descElement = document.querySelector(".temperature-description p");
 
 // App data
 const weather = {};
 weather.temperature = {
-    unit: 'celsius',
+  unit: "celsius",
 };
 
-var tempUnit = 'F';
+var tempUnit = "F";
 
 const KELVIN = 273.15;
-const key = '88d2049f3b9bec8eb671985c015041e8';
+const key = "88d2049f3b9bec8eb671985c015041e8";
 
 // Set Position function
 setPosition();
 
 function setPosition(position) {
-    // Here you can change your position
-    let latitude = 32.715736;
-    let longitude = -117.161087;
+  // Here you can change your position
+  let latitude = 32.715736;
+  let longitude = -117.161087;
 
-    getWeather(latitude, longitude);
+  getWeather(latitude, longitude);
 }
 
 // Get the Weather data
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+  let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
-    console.log(api);
+  console.log(api);
 
-    fetch(api)
-        .then(function (response) {
-            let data = response.json();
-            return data;
-        })
-        .then(function (data) {
-            let celsius = Math.floor(data.main.temp - KELVIN);
-            weather.temperature.value = (tempUnit == 'C') ? celsius : (celsius * 9/5) + 32;
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-        })
-        .then(function () {
-            displayWeather();
-        });
+  fetch(api)
+    .then(function (response) {
+      let data = response.json();
+      return data;
+    })
+    .then(function (data) {
+      let celsius = Math.floor(data.main.temp - KELVIN);
+      weather.temperature.value =
+        tempUnit == "C" ? celsius : (celsius * 9) / 5 + 32;
+      weather.description = data.weather[0].description;
+      weather.iconId = data.weather[0].icon;
+    })
+    .then(function () {
+      displayWeather();
+    });
 }
 
 // Display Weather info
 function displayWeather() {
-    iconElement.innerHTML = `<img src="icons/OneDark/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
-    descElement.innerHTML = weather.description;
+  iconElement.innerHTML = `<img src="icons/OneDark/${weather.iconId}.png"/>`;
+  tempElement.innerHTML = `${weather.temperature.value}°<span class="darkfg">${tempUnit}</span>`;
+  descElement.innerHTML = weather.description;
 }
